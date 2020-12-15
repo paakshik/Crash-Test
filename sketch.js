aef2f7dd78250efab356525aa19db6b737a34034
paakshik <74145408+paakshik@users.noreply.github.com>
2020-12-15 16:49:10 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,13 +21,15 @@ function setup() {
   car.scale = 0.5;
   wall = createSprite(730,200,20,300);
   wall.shapeColor = "red";
+   alert('Press the Y key to make the car move');
+    alert('Press the up key to stop the crash test!!')
 }
 
 function draw() {
-  background(0);  
+  background(0); 
+  
   if (flag === "Working"){
-   alert('Press the Y key to make the car move');
-    alert('Press the up key to stop the crash test!!')
+  
   speed = random(55,90);
   weight = random(10,1500);
   if (keyWentDown("y")){
